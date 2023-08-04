538ae3dcab19dc02fbc0723a310740592953064c
Yasmin Lopes <yasmin.lopesx27@gmail.com>
2023-08-04 15:19:37 -0300

    refactor: change import

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
-import { ServerError } from '../errors/server-error'
-import type { HttpResponse } from '../protocols/http'
+import { ServerError } from '../errors'
+import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
