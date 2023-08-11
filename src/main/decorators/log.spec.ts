e25d434acd8ad7b4357f2fb7c587b4e4bd12ea28
Yasmin Lopes <yclopes27@gmail.com>
2023-08-11 14:19:36 -0300

    test: ensure LogControllerDecorator returns the same results of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -46,4 +46,24 @@ describe('Log Controller Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('shouldreturn the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Yasmin'
+      }
+    })
+  })
 })
