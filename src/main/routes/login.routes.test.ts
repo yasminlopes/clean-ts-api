cb5d61d83bf048cdda955a9de15420963df5a8ea
Yasmin Lopes <yclopes27@gmail.com>
2023-10-02 11:52:47 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'yasmin.lopesx27@gmail.com',
+          password: 'password'
+        })
+        .expect(401)
+    })
   })
 })
